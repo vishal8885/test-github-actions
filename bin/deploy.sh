59899427de78cde7db3f928d6f8c6343aadfa37f
vishal <vishalmudhiraj71@gmail.com>
2021-11-09 16:26:31 +0530

    test

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
 newURL="deploy branch URL"
 echo "test : old url"
+URL=${URL/github.com/api.github.com/repos}"/issues/${PR}/comments"
 curl -X POST $URL -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" --data '{ "body": "'"$newURL"'" }'
